85416b5b4263966f480b464ab323bc544bee9a7d
Joshua Harrison <harrj211@hsd.k12.or.us>
2023-10-16 15:44:30 -0700

    Moved the crosshair to a usable spot

--- Assets/scripts/LaunchProjectile.cs
+++ Assets/scripts/LaunchProjectile.cs
@@ -14,7 +14,7 @@ public class LaunchProjectile : MonoBehaviour
         if (Input.GetButton("Fire1"))
         {
             GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
-            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, launchVelocity, 0));
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, 0, launchVelocity));
         }
 
 
